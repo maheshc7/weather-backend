014daaf091188ecb6ee8625d93feea7fc2ec1abe
Mahesh <mrak4123@gmail.com>
2022-02-04 20:12:09 -0800

    login returns access token

--- app.js
+++ app.js
@@ -2,6 +2,9 @@ const express = require('express')
 const app = express()
 const port = 3000
 const data = require('./weather.json')
+var bodyParser = require('body-parser');
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.json());
 
 app.use(function (req, res, next) {
     // Website you wish to allow to connect
